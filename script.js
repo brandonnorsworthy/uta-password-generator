a36945bb6234b27c7bf9685512de59df0fad6ec8
Brandon Norsworthy <brandonknorsworthy@gmail.com>
2021-06-22 18:50:10 -0500

    added support for interpolating variables into strings for dynamic changes and setup password creation

--- script.js
+++ script.js
@@ -13,32 +13,50 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+var password = {
+  passwordLength: 0,
+  lowercaseChars: null,
+  uppercaseChars: null,
+  numericChars: null,
+  specialChars: null
+}
 
 //click button generate password
 //generatePassword() main function doing work
 // Gets called when the Generate Password btn is clicked return string to get displayed in the text area
 function generatePassword() {
-  var password = 'no password 4 u... yet lul'; //initialize password
+  var password = ``; //initialize password empty string so can concatenate to it
 
   //prompt length variables
-  var passwordLength = 0; //initialize passwordLength for while loop
-  var passwordMinLength = 8; //intialize at top for easy access and reading
-  var passwordMaxLength = 128; //intialize at top for easy access and reading
+  const passwordMinLength = 8; //intialize at top for easy access and reading
+  const passwordMaxLength = 128; //intialize at top for easy access and reading
+  //prompts for the user
+  var passwordPreferencePrompts = [
+    `Length of the password to generate.\nBetween ${passwordMinLength} and ${passwordMaxLength} Characters`, //prompt window (textbox for number)
+    `Lowercase.\nExample: ipsum`,
+    `Uppercase.\nExample: LOREM`,
+    `Numbers.\nExample: 12345`,
+    `Special Characters.\nExample: /?!$%`
+  ];
 
-  //prompt character choice variables
-  var passwordPreferencePrompts = ["Lowercase... ipsum", "Uppercase... LOREM", "Numbers... 12345", "Special Characters... /?!$%"];
-  var passwordPreferenceChosen;
+  //user variables
+  var passwordLength = 0;
+  var passwordPreferenceChosen = []; //initialize empty array
 
+  //text box prompt to take number of characters in password
   while (!(passwordMinLength <= passwordLength) || !(passwordLength <= passwordMaxLength)){ //if passwordLength is too small or too large ask again deals with characters too
-    passwordLength = Number(window.prompt('Length of the password to generate. Between 8 and 128 Characters')); //intialize passwordLength by using window prompt and convert response to a number
+    passwordLength = (Number(window.prompt(passwordPreferencePrompts[0]))); //assign passwordLength by using window prompt and convert response to a number
   }
 
-  //lowercase, uppercase, numeric, and/or special characters
-  window.confirm("ok or cancels stuff");
-  lowerCaseChars = true;
-  upperCaseChars = true;
-  numericChars = true;
-  specialChars = true;
+  // True False prompts... lowercase, uppercase, numeric, and/or special characters
+  for (let index = 1; index < passwordPreferencePrompts.length; index++) { //loop through all possible prompts (more can be added at anytime)
+    passwordPreferenceChosen.push(window.confirm(`Would you like to include: ` + passwordPreferencePrompts[index])); //using index show prompts and add users choice into the passwordPreferenceChosen array for later use
+  }
+
+  //create password
+  for (let index = 0; index < passwordLength; index++) {
+    password += 'a'
+  }
   //after returning password shows in box
   return password;
 }
