8d55e544e1a18184385ba29d17ae121dd26f188e
Brandon Norsworthy <brandonknorsworthy@gmail.com>
2021-06-22 17:45:37 -0500

    javascript basic puesdo and code mixture just playing around

--- script.js
+++ script.js
@@ -12,3 +12,26 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+//click button generate password
+//generatePassword() main function doing work
+// Gets called when the Generate Password btn is clicked return string to get displayed in the text area
+function generatePassword() {
+  password = 'no password 4 u... yet lul'; //initialize password
+
+/*   //series of prompts alerts("")
+  alert("this is a alert"); */
+
+  //prompt for the length of password RANGE [8chars -> 128chars]
+  window.prompt("this is a prompt");
+  //character types to include in the password
+  passwordLength = 8;
+  //lowercase, uppercase, numeric, and/or special characters
+  lowerCaseChars = true;
+  upperCaseChars = true;
+  numericChars = true;
+  specialChars = true;
+  //after returning password shows in box
+  return password;
+}
