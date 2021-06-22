23efebffe31e79de7cf8884f17f7f01012dde795
Brandon Norsworthy <brandonknorsworthy@gmail.com>
2021-06-22 18:03:48 -0500

    password length dealt with using while loop

--- script.js
+++ script.js
@@ -18,16 +18,28 @@ generateBtn.addEventListener("click", writePassword);
 //generatePassword() main function doing work
 // Gets called when the Generate Password btn is clicked return string to get displayed in the text area
 function generatePassword() {
-  password = 'no password 4 u... yet lul'; //initialize password
+  //initialize password
+  var password = 'no password 4 u... yet lul';
 
-/*   //series of prompts alerts("")
-  alert("this is a alert"); */
+  var passwordMinLength = 8;
+  var passwordMaxLength = 128;
+
+  var passwordPreferencePrompts = ["Lowercase... ipsum", "Uppercase... LOREM", "Numbers... 12345", "Special Characters... /?!$%"];
+  var passwordPreferenceChosen;
+
+  //series of prompts alerts("")
 
   //prompt for the length of password RANGE [8chars -> 128chars]
-  window.prompt("this is a prompt");
   //character types to include in the password
-  passwordLength = 8;
+
+  var passwordLength = Number(window.prompt("Length of the password to generate. Between 8 and 128 Characters")); //intialize passwordLength by using window prompt and convert response to a number
+
+  while (!(passwordMinLength <= passwordLength) || !(passwordLength <= passwordMaxLength)){ //if passwordLength is too small or too large ask again deals with characters too
+    passwordLength = Number(window.prompt("Length of the password to generate. Between 8 and 128 Characters"));
+  }
+1
   //lowercase, uppercase, numeric, and/or special characters
+  window.confirm("ok or cancels stuff");
   lowerCaseChars = true;
   upperCaseChars = true;
   numericChars = true;
